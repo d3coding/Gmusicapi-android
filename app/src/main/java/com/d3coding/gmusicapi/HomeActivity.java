8282b86d9f2c83d8933061247131080c4dc229df
d3coding <mateusjuniordev@gmail.com>
2019-07-05 16:27:28 -0300

    Created HomeFragment for best iteration with Navigation Drawer.

--- app/src/main/java/com/d3coding/gmusicapi/HomeActivity.java
+++ app/src/main/java/com/d3coding/gmusicapi/HomeActivity.java
@@ -4,69 +4,29 @@ import android.Manifest;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
-import android.graphics.Bitmap;
-import android.graphics.Color;
-import android.graphics.drawable.GradientDrawable;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
+import android.support.annotation.NonNull;
 import android.support.design.widget.NavigationView;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.FileProvider;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.graphics.Palette;
-import android.support.v7.widget.DefaultItemAnimator;
-import android.support.v7.widget.LinearLayoutManager;
-import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.view.animation.AnimationUtils;
-import android.view.animation.LayoutAnimationController;
-import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.LinearLayout;
-import android.widget.RadioGroup;
-import android.widget.TextView;
 import android.widget.Toast;
 
-import com.d3coding.gmusicapi.items.MusicAdapter;
-import com.d3coding.gmusicapi.items.MusicItem;
-
-import java.io.File;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Future;
 
 public class HomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
-    private static final int DOWNLOADS_NUM = 5;
 
     static final int LOGIN_ACTIVITY = 11;
 
-    private GMusicDB db;
-    private GMusicFile gmusicFile;
     private boolean mState = false;
     private DrawerLayout drawer;
     private NavigationView navigationView;
-    private RecyclerView recyclerView;
-    private List<MusicItem> ConvertList = new ArrayList<>();
-    private MusicAdapter mAdapter;
-
-    private GMusicDB.column sort = GMusicDB.column.title;
-    private GMusicDB.SortOnline sortOnline = GMusicDB.SortOnline.all;
-
-    private ExecutorService downloadQueueService;
-    private Download downloadQueue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -83,140 +43,18 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
             setContentView(R.layout.ac_home);
 
             Toolbar toolbar = findViewById(R.id.toolbar);
-            recyclerView = findViewById(R.id.music_item);
+
             drawer = findViewById(R.id.drawer_layout);
             navigationView = findViewById(R.id.nav_view);
 
             setSupportActionBar(toolbar);
 
-            {
-
-                gmusicFile = new GMusicFile(this);
-                mAdapter = new MusicAdapter(ConvertList);
-
-                int resId = R.anim.layout_animation_fall_down;
-                LayoutAnimationController animation = AnimationUtils.loadLayoutAnimation(this, resId);
-                recyclerView.setLayoutAnimation(animation);
-                recyclerView.setLayoutManager(new LinearLayoutManager(this));
-                recyclerView.setItemAnimator(new DefaultItemAnimator());
-                recyclerView.setAdapter(mAdapter);
-
-                {
-                    if (db == null)
-                        db = new GMusicDB(this);
-
-                    // TODO: scanFilesAndUpdateDB
-
-                    ConvertList.addAll(db.getMusicItems(sort, sortOnline, "", false));
-                    mAdapter.notifyDataSetChanged();
-                }
-
-                mAdapter.setOnItemClickListener((view, position) -> {
-
-                    Bitmap bitmap = gmusicFile.getBitmapThumbImage(ConvertList.get(position).getUUID());
-                    if (bitmap == null)
-                        bitmap = gmusicFile.getDefaultThumb();
-
-                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
-
-                    ViewGroup vView = (ViewGroup) getLayoutInflater().inflate(R.layout.ad_music_info, null);
-
-                    ((ImageView) vView.findViewById(R.id.info_albumArt)).setImageBitmap(bitmap);
-                    Palette p = Palette.from(bitmap).generate();
-
-                    TextView textView = vView.findViewById(R.id.info_title);
-
-                    textView.setText(ConvertList.get(position).getTitle());
-                    textView.setTextColor(p.getDarkVibrantColor(Color.BLACK));
-                    ((GradientDrawable) textView.getBackground()).setColor(p.getLightVibrantColor(Color.WHITE));
-
-                    vView.findViewById(R.id.info_background).setBackgroundColor(p.getMutedColor(Color.WHITE));
-
-                    ((TextView) vView.findViewById(R.id.info_album)).setText(ConvertList.get(position).getAlbum());
-                    ((TextView) vView.findViewById(R.id.info_artist)).setText(ConvertList.get(position).getArtist());
-                    ((TextView) vView.findViewById(R.id.info_time)).setText(ConvertList.get(position).getDuration());
-
-                    builder.setView(vView).create().show();
-
-                });
-
-                mAdapter.setOnItemLongClickListener((view, position) -> {
-                    MusicItem musicItem = ConvertList.get(position);
-
-                    Bitmap bitmap = gmusicFile.getBitmapThumbImage(musicItem.getUUID());
-                    if (bitmap == null)
-                        bitmap = gmusicFile.getDefaultThumb();
-
-                    AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                    ViewGroup vView = (ViewGroup) getLayoutInflater().inflate(R.layout.ad_music_opt, null);
-
-                    LinearLayout linearComplete = vView.findViewById(R.id.status_complete),
-                            linearDownloading = vView.findViewById(R.id.status_downloading);
-
-                    ((ImageView) vView.findViewById(R.id.opt_album_art)).setImageBitmap(bitmap);
-                    ((TextView) vView.findViewById(R.id.opt_title)).setText(musicItem.getTitle());
-                    ((TextView) vView.findViewById(R.id.opt_artist)).setText(musicItem.getArtist());
-
-                    if (db.countDownloadsByUUID(musicItem.getUUID()) > 0) {
-                        linearComplete.setVisibility(View.VISIBLE);
-                        linearDownloading.setVisibility(View.GONE);
-                    } else {
-                        // TODO: CheckNetwork
-                        linearComplete.setVisibility(View.GONE);
-                        linearDownloading.setVisibility(View.VISIBLE);
-
-                        if (downloadQueueService == null)
-                            downloadQueueService = Executors.newFixedThreadPool(DOWNLOADS_NUM);
-                        if (downloadQueue == null)
-                            downloadQueue = new Download();
-
-                        if (!downloadQueue.hasUUID(musicItem.getUUID())) {
-                            downloadQueue.addQueueService(musicItem.getUUID(),
-                                    downloadQueueService.submit(() -> {
-                                        // TODO: getDownloadStatus
-                                        if (gmusicFile.getQueue(musicItem.getUUID()) != 0)
-                                            synchronized (this) {
-                                                runOnUiThread(() -> {
-                                                    linearComplete.setVisibility(View.VISIBLE);
-                                                    linearDownloading.setVisibility(View.GONE);
-                                                    mAdapter.notifyItemChanged(position);
-                                                    downloadQueue.setComplete(musicItem.getUUID());
-                                                });
-                                            }
-                                    })
-                            );
-                        } else {
-                            // TODO: getStatus
-                        }
-                    }
-
-                    vView.findViewById(R.id.opt_bt_play).setOnClickListener((view2) -> {
-                        Uri uri = FileProvider.getUriForFile(this, this.getPackageName() + ".fileprovider",
-                                new File(new File(Environment.getExternalStorageDirectory(), "Gmusicapi"), musicItem.getUUID() + ".mp3"));
-
-                        Intent intent = new Intent(Intent.ACTION_VIEW);
-                        intent.setDataAndType(uri, getContentResolver().getType(uri))
-                                .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
-                        startActivity(intent);
-
-                    });
-
-                    vView.findViewById(R.id.opt_bt_open).setOnClickListener((view2) ->
-                            startActivity(new Intent(Intent.ACTION_VIEW).setDataAndType(Uri.parse(Environment.getExternalStorageDirectory() + "/Gmusicapi/"), "resource/folder")));
-
-                    vView.findViewById(R.id.opt_bt_delete).setOnClickListener((view2) -> Toast.makeText(this, getString(R.string.null_description), Toast.LENGTH_SHORT).show());
-
-                    builder.setView(vView).create().show();
-
-                });
-
-            }
-
-            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this,
-                    drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+            ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
             drawer.addDrawerListener(toggle);
             toggle.syncState();
             navigationView.setNavigationItemSelectedListener(this);
+
+            getSupportFragmentManager().beginTransaction().add(R.id.fragment_content, new HomeFragment()).commit();
 
             if (!mPresets.contains(getString(R.string.last_update)))
                 refreshDB();
@@ -228,46 +66,6 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         } else
             startActivityForResult(new Intent(this, LoginActivity.class), LOGIN_ACTIVITY);
 
-    }
-
-    class Download {
-        private List<String> UUID;
-        private List<Future> future;
-
-        Download() {
-            this.UUID = new ArrayList<>();
-            this.future = new ArrayList<>();
-        }
-
-        int addQueueService(String UUID, Future future) {
-            this.UUID.add(UUID);
-            this.future.add(future);
-            return this.UUID.size() - 1;
-        }
-
-        boolean hasUUID(String UUID) {
-            for (String uuid : this.UUID)
-                if (UUID.equals(uuid))
-                    return true;
-            return false;
-        }
-
-        void setComplete(String UUID) {
-            for (int x = 0; x < this.UUID.size(); ++x)
-                if (this.UUID.equals(UUID)) {
-                    this.UUID.remove(x);
-                    this.future.remove(x);
-                    break;
-                }
-        }
-
-        public String getUUID(int position) {
-            return UUID.get(position);
-        }
-
-        public Future getFuture(int position) {
-            return future.get(position);
-        }
     }
 
     @Override
@@ -303,46 +101,7 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
             Toast.makeText(this, getString(R.string.null_description), Toast.LENGTH_SHORT).show();
             return true;
         } else if (id == R.id.act_icon_filter) {
-            {
-                AlertDialog.Builder builder = new AlertDialog.Builder(this);
-                ViewGroup vView = (ViewGroup) getLayoutInflater().inflate(R.layout.ad_filter, null);
-
-                builder.setPositiveButton(R.string.act_icon_filter, (dialog, which) -> {
-
-                    if (db == null)
-                        db = new GMusicDB(this);
-
-                    int x = ((RadioGroup) vView.findViewById(R.id.radioGroup1)).getCheckedRadioButtonId();
-                    int y = ((RadioGroup) vView.findViewById(R.id.radioGroup2)).getCheckedRadioButtonId();
-
-                    if (x == R.id.radio_title)
-                        sort = GMusicDB.column.title;
-                    else if (x == R.id.radio_artist)
-                        sort = GMusicDB.column.artist;
-                    else if (x == R.id.radio_album)
-                        sort = GMusicDB.column.album;
-                    else if (x == R.id.radio_genre)
-                        sort = GMusicDB.column.genre;
-                    else
-                        sort = null;
-
-                    if (y == R.id.radio_all) {
-                        sortOnline = GMusicDB.SortOnline.all;
-                    } else if (y == R.id.radio_online) {
-                        sortOnline = GMusicDB.SortOnline.online;
-                    } else if (y == R.id.radio_offline) {
-                        sortOnline = GMusicDB.SortOnline.offline;
-                    } else
-                        sortOnline = null;
-
-                    ConvertList.clear();
-                    ConvertList.addAll(db.getMusicItems(sort, sortOnline, ((EditText) vView.findViewById(R.id.filter_text)).getText().toString(), false));
-
-                    mAdapter.notifyDataSetChanged();
-
-                }).setView(vView).create().show();
-
-            }
+            ((HomeFragment) getSupportFragmentManager().findFragmentById(R.id.fragment_content)).showFilter();
             return true;
         } else if (id == R.id.action_clean_db) {
             deleteDatabase(GMusicDB.DATABASE_NAME);
@@ -387,9 +146,9 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
     }
 
     @Override
-    public boolean onNavigationItemSelected(MenuItem item) {
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
         // Handle navigation view item clicks here.
-        int id = item.getItemId();
+        int id = menuItem.getItemId();
 
         if (id == R.id.nav_all) {
             // TODO: HomeActivity
@@ -411,6 +170,5 @@ public class HomeActivity extends AppCompatActivity implements NavigationView.On
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
-
-
 }
+
---
+++ app/src/main/java/com/d3coding/gmusicapi/HomeFragment.java
@@ -0,0 +1,274 @@
+package com.d3coding.gmusicapi;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Color;
+import android.graphics.drawable.GradientDrawable;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.graphics.Palette;
+import android.support.v7.widget.DefaultItemAnimator;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.d3coding.gmusicapi.items.MusicAdapter;
+import com.d3coding.gmusicapi.items.MusicItem;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+
+public class HomeFragment extends Fragment {
+
+    private static final int DOWNLOADS_NUM = 5;
+
+    private GMusicDB db;
+
+    private GMusicDB.column sort = GMusicDB.column.title;
+    private GMusicDB.SortOnline sortOnline = GMusicDB.SortOnline.all;
+
+    private List<MusicItem> ConvertList = new ArrayList<>();
+    private MusicAdapter mAdapter;
+
+    private RecyclerView recyclerView;
+
+    private GMusicFile gmusicFile;
+
+    private ExecutorService downloadQueueService;
+    private Download downloadQueue;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.frag_items, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View layoutView, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(layoutView, savedInstanceState);
+
+        recyclerView = layoutView.findViewById(R.id.items);
+        gmusicFile = new GMusicFile(getContext());
+        mAdapter = new MusicAdapter(ConvertList);
+
+        LayoutAnimationController animation = AnimationUtils.loadLayoutAnimation(getContext(), R.anim.layout_animation_fall_down);
+        recyclerView.setLayoutAnimation(animation);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setAdapter(mAdapter);
+
+        // TODO: scanFilesAndUpdateDB
+        db = new GMusicDB(getContext());
+
+        ConvertList.addAll(db.getMusicItems(sort, sortOnline, "", false));
+        mAdapter.notifyDataSetChanged();
+
+        mAdapter.setOnItemClickListener((view, position) -> {
+
+            Bitmap bitmap = gmusicFile.getBitmapThumbImage(ConvertList.get(position).getUUID());
+            if (bitmap == null)
+                bitmap = gmusicFile.getDefaultThumb();
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+
+            ViewGroup vView = (ViewGroup) getLayoutInflater().inflate(R.layout.ad_music_info, null);
+
+            ((ImageView) vView.findViewById(R.id.info_albumArt)).setImageBitmap(bitmap);
+            Palette p = Palette.from(bitmap).generate();
+
+            TextView textView = vView.findViewById(R.id.info_title);
+
+            textView.setText(ConvertList.get(position).getTitle());
+            textView.setTextColor(p.getDarkVibrantColor(Color.BLACK));
+            ((GradientDrawable) textView.getBackground()).setColor(p.getLightVibrantColor(Color.WHITE));
+
+            vView.findViewById(R.id.info_background).setBackgroundColor(p.getMutedColor(Color.WHITE));
+
+            ((TextView) vView.findViewById(R.id.info_album)).setText(ConvertList.get(position).getAlbum());
+            ((TextView) vView.findViewById(R.id.info_artist)).setText(ConvertList.get(position).getArtist());
+            ((TextView) vView.findViewById(R.id.info_time)).setText(ConvertList.get(position).getDuration());
+
+            builder.setView(vView).create().show();
+
+        });
+
+        mAdapter.setOnItemLongClickListener((view, position) -> {
+            MusicItem musicItem = ConvertList.get(position);
+
+            Bitmap bitmap = gmusicFile.getBitmapThumbImage(musicItem.getUUID());
+            if (bitmap == null)
+                bitmap = gmusicFile.getDefaultThumb();
+
+            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+            ViewGroup vView = (ViewGroup) getLayoutInflater().inflate(R.layout.ad_music_opt, null);
+
+            LinearLayout linearComplete = vView.findViewById(R.id.status_complete),
+                    linearDownloading = vView.findViewById(R.id.status_downloading);
+
+            ((ImageView) vView.findViewById(R.id.opt_album_art)).setImageBitmap(bitmap);
+            ((TextView) vView.findViewById(R.id.opt_title)).setText(musicItem.getTitle());
+            ((TextView) vView.findViewById(R.id.opt_artist)).setText(musicItem.getArtist());
+
+            if (db.countDownloadsByUUID(musicItem.getUUID()) > 0) {
+                linearComplete.setVisibility(View.VISIBLE);
+                linearDownloading.setVisibility(View.GONE);
+            } else {
+                // TODO: CheckNetwork
+                linearComplete.setVisibility(View.GONE);
+                linearDownloading.setVisibility(View.VISIBLE);
+
+                if (downloadQueueService == null)
+                    downloadQueueService = Executors.newFixedThreadPool(DOWNLOADS_NUM);
+                if (downloadQueue == null)
+                    downloadQueue = new Download();
+
+                if (!downloadQueue.hasUUID(musicItem.getUUID())) {
+                    downloadQueue.addQueueService(musicItem.getUUID(),
+                            downloadQueueService.submit(() -> {
+                                // TODO: getDownloadStatus
+                                if (gmusicFile.getQueue(musicItem.getUUID()) != 0)
+                                    synchronized (this) {
+                                        ((Activity) getContext()).runOnUiThread(() -> {
+                                            linearComplete.setVisibility(View.VISIBLE);
+                                            linearDownloading.setVisibility(View.GONE);
+                                            mAdapter.notifyItemChanged(position);
+                                            downloadQueue.setComplete(musicItem.getUUID());
+                                        });
+                                    }
+                            })
+                    );
+                } else {
+                    // TODO: getStatus
+                }
+            }
+
+            vView.findViewById(R.id.opt_bt_play).setOnClickListener((view2) -> {
+                Uri uri = FileProvider.getUriForFile(getContext(), getContext().getPackageName() + ".fileprovider",
+                        new File(new File(Environment.getExternalStorageDirectory(), "Gmusicapi"), musicItem.getUUID() + ".mp3"));
+
+                Intent intent = new Intent(Intent.ACTION_VIEW);
+                intent.setDataAndType(uri, getContext().getContentResolver().getType(uri))
+                        .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION);
+                startActivity(intent);
+
+            });
+
+            vView.findViewById(R.id.opt_bt_open).setOnClickListener((view2) ->
+                    startActivity(new Intent(Intent.ACTION_VIEW).setDataAndType(Uri.parse(Environment.getExternalStorageDirectory() + "/Gmusicapi/"), "resource/folder")));
+
+            vView.findViewById(R.id.opt_bt_delete).setOnClickListener((view2) -> Toast.makeText(getContext(), getString(R.string.null_description), Toast.LENGTH_SHORT).show());
+
+            builder.setView(vView).create().show();
+
+        });
+
+
+    }
+
+    void showFilter() {
+        {
+            AlertDialog.Builder builder = new AlertDialog.Builder(getContext());
+            ViewGroup vView = (ViewGroup) getLayoutInflater().inflate(R.layout.ad_filter, null);
+
+            builder.setPositiveButton(R.string.act_icon_filter, (dialog, which) -> {
+
+                if (db == null)
+                    db = new GMusicDB(getContext());
+
+                int x = ((RadioGroup) vView.findViewById(R.id.radioGroup1)).getCheckedRadioButtonId();
+                int y = ((RadioGroup) vView.findViewById(R.id.radioGroup2)).getCheckedRadioButtonId();
+
+                if (x == R.id.radio_title)
+                    sort = GMusicDB.column.title;
+                else if (x == R.id.radio_artist)
+                    sort = GMusicDB.column.artist;
+                else if (x == R.id.radio_album)
+                    sort = GMusicDB.column.album;
+                else if (x == R.id.radio_genre)
+                    sort = GMusicDB.column.genre;
+                else
+                    sort = null;
+
+                if (y == R.id.radio_all) {
+                    sortOnline = GMusicDB.SortOnline.all;
+                } else if (y == R.id.radio_online) {
+                    sortOnline = GMusicDB.SortOnline.online;
+                } else if (y == R.id.radio_offline) {
+                    sortOnline = GMusicDB.SortOnline.offline;
+                } else
+                    sortOnline = null;
+
+                ConvertList.clear();
+                ConvertList.addAll(db.getMusicItems(sort, sortOnline, ((EditText) vView.findViewById(R.id.filter_text)).getText().toString(), false));
+
+                mAdapter.notifyDataSetChanged();
+
+            }).setView(vView).create().show();
+
+        }
+
+    }
+
+    class Download {
+        private List<String> UUID;
+        private List<Future> future;
+
+        Download() {
+            this.UUID = new ArrayList<>();
+            this.future = new ArrayList<>();
+        }
+
+        int addQueueService(String UUID, Future future) {
+            this.UUID.add(UUID);
+            this.future.add(future);
+            return this.UUID.size() - 1;
+        }
+
+        boolean hasUUID(String UUID) {
+            for (String uuid : this.UUID)
+                if (UUID.equals(uuid))
+                    return true;
+            return false;
+        }
+
+        void setComplete(String UUID) {
+            for (int x = 0; x < this.UUID.size(); ++x)
+                if (this.UUID.get(x).equals(UUID)) {
+                    this.UUID.remove(x);
+                    this.future.remove(x);
+                    break;
+                }
+        }
+
+        public String getUUID(int position) {
+            return UUID.get(position);
+        }
+
+        public Future getFuture(int position) {
+            return future.get(position);
+        }
+
+    }
+
+}
